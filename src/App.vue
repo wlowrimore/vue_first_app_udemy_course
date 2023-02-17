9a2edaf394adcb09d38761c7f4985ea6ac8fa79e
William Lowrimore <wlowrimore@gmail.com>
2023-02-17 12:41:06 -0600

    adds and connects main App.vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data9 () {
+    return {
+      friends: [
+        {
+          id: 'nikki',
+          name: 'Nikki Erwin',
+          phone: '123 456 7890',
+          email: 'lovelylady@gmail.com'
+        },
+        {
+          id: 'kevin',
+          name: 'Kevin Kensley',
+          phone: '987 654 3210',
+          email: 'snipesac@gmail.com'
+        }
+      ]
+    }
+  }
+}
+</script>
