4c2e5bcf763d4aeb7268ebfc54e123929398fcf4
William Lowrimore <wlowrimore@gmail.com>
2023-02-17 12:57:15 -0600

    adds unstyled FriendContact component with friend-contact tag

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <section>
     <h2>My Friends</h2>
     <ul>
-      <li></li>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
     </ul>
   </section>
 </template>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'nikki',
+        name: 'Nikki Erwin',
+        phone: '123 456 7890',
+        email: 'lovelylady@gmail.com'
+      }
+    }
+  },
+  methods: {
+    toggleDetails () {
+      this.detailsAreVisible = !this.detailsAreVisible
+    }
+  }
+}
+</script>
